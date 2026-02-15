59df2be9015a8973ae7fc38db99e7bdc8c983a50
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-02-15 16:22:18 +0800

    update init.el

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
 ;;; init.el --- Init -*- lexical-binding: t; no-byte-compile: t; -*-
-;;
+
 ;; Author: simplyshiro
 ;; URL: https://github.com/simplyshiro/.emacs.d
-;;
+
 ;;; Commentary:
-;;
+
 ;;; Code:
 
 (add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
