83a14a49733e99d9ae5cbc629233482e170ca123
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-04-19 02:28:30 +0800

    update early-init.el

--- early-init.el
+++ early-init.el
@@ -25,50 +25,50 @@
   :group 'shiro
   :type 'directory)
 
-(setopt backup-directory-alist
+(setq backup-directory-alist
         `(("." . ,(expand-file-name "backups/" shiro-variable-directory))))
 
 (startup-redirect-eln-cache
  (expand-file-name "eln-cache/" shiro-variable-directory))
 
-(setopt custom-theme-directory
+(setq custom-theme-directory
         (expand-file-name "themes/" shiro-variable-directory))
-(setopt treesit--install-language-grammar-out-dir-history
+(setq treesit--install-language-grammar-out-dir-history
         (list (expand-file-name "tree-sitter/" shiro-variable-directory)))
-(setopt treesit-extra-load-path
+(setq treesit-extra-load-path
         (list (expand-file-name "tree-sitter/" shiro-variable-directory)))
 
-(setopt auto-save-list-file-prefix
+(setq auto-save-list-file-prefix
         (expand-file-name "auto-save-list/.saves-" shiro-variable-directory))
-(setopt transient-history-file
+(setq transient-history-file
         (expand-file-name "transient/history.el" shiro-variable-directory))
 
-(setopt custom-file (expand-file-name "custom.el" shiro-variable-directory))
-(setopt savehist-file (expand-file-name "history" shiro-variable-directory))
-(setopt save-place-file (expand-file-name "places" shiro-variable-directory))
-(setopt project-list-file
+(setq custom-file (expand-file-name "custom.el" shiro-variable-directory))
+(setq savehist-file (expand-file-name "history" shiro-variable-directory))
+(setq save-place-file (expand-file-name "places" shiro-variable-directory))
+(setq project-list-file
         (expand-file-name "projects" shiro-variable-directory))
-(setopt recentf-save-file
+(setq recentf-save-file
         (expand-file-name "recentf" shiro-variable-directory))
-(setopt tramp-persistency-file-name
+(setq tramp-persistency-file-name
         (expand-file-name "tramp" shiro-variable-directory))
 
 (setq package-enable-at-startup nil)
 
-(setopt load-prefer-newer t)
+(setq load-prefer-newer t)
 
-(setopt inhibit-startup-buffer-menu t)
-(setopt inhibit-startup-echo-area-message user-login-name)
-(setopt inhibit-startup-screen t)
-(setopt initial-buffer-choice nil)
-(setopt initial-major-mode 'fundamental-mode)
-(setopt initial-scratch-message nil)
+(setq inhibit-startup-buffer-menu t)
+(setq inhibit-startup-echo-area-message user-login-name)
+(setq inhibit-startup-screen t)
+(setq initial-buffer-choice nil)
+(setq initial-major-mode 'fundamental-mode)
+(setq initial-scratch-message nil)
 
-(setopt frame-inhibit-implied-resize t)
-(setopt frame-resize-pixelwise t)
+(setq frame-inhibit-implied-resize t)
+(setq frame-resize-pixelwise t)
 
 (when (boundp 'pgtk-wait-for-event-timeout)
-  (setopt pgtk-wait-for-event-timeout 0.001))
+  (setq pgtk-wait-for-event-timeout 0.001))
 
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
