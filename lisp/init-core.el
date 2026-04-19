50c2dcd9c8dbb5683b62d015da78b1a719893ff8
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-04-19 23:54:30 +0800

    update init-core.el

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -120,7 +120,7 @@
               ("M-A" . marginalia-cycle)))
 
 (use-package embark
-  :init (setopt prefix-help-command #'embark-prefix-help-command)
+  :init (setq prefix-help-command #'embark-prefix-help-command)
   :config (add-to-list 'display-buffer-alist
                        '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                          nil (window-parameters (mode-line-format . none))))
