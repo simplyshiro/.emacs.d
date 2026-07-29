ade67f18e42bb26f93cdecc1afe30e1668ecdbae
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-07-29 21:28:34 +0800

    update init-dev.el

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -18,7 +18,10 @@
                '((python-base-mode) "rass" "python"))
   (add-to-list 'eglot-server-programs '(qml-ts-mode "qmlls6"))
   (add-to-list 'eglot-server-programs
-               '((rust-mode rust-ts-mode) "rust-analyzer" :initializationOptions (:check (:command "clippy"))))
+               '((rust-mode rust-ts-mode)
+                 "rust-analyzer" :initializationOptions
+                 (:check
+                  (:command "clippy"))))
   (setq completion-category-defaults nil)
   :hook
   ((c-mode-hook c-ts-mode-hook) . eglot-ensure)
