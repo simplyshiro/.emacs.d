e643d48ba2f90ebc9b5b481b91455841b20f2840
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-08-21 16:17:35 +0800

    update init-core.el

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -1,4 +1,4 @@
-;;; init-core.el --- Init Core -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; init-core.el --- Init Core -*- lexical-binding: t; -*-
 
 ;; Author: simplyshiro
 ;; URL: https://github.com/simplyshiro/.emacs.d
