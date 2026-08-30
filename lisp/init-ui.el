148ac9a52986d8a5f00276772f8878e8a32f0203
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-08-30 19:06:12 +0800

    update init-ui.el

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -7,35 +7,6 @@
 
 ;;; Code:
 
-(defcustom shiro-theme 'doric-light
-  "Theme to load on startup."
-  :tag "shiro Theme"
-  :type 'symbol
-  :group 'shiro)
-
-(defun shiro--load-theme ()
-  "Load `shiro-theme'."
-  (unless (custom-theme-enabled-p shiro-theme)
-    (mapc #'disable-theme custom-enabled-themes)
-    (load-theme shiro-theme :no-confirm)))
-
-(add-hook 'elpaca-after-init-hook #'shiro--load-theme)
-
-(use-package doric-themes
-  :demand t)
-
-(use-package ef-themes
-  :demand t)
-
-(use-package modus-themes
-  :config
-  (setq modus-themes-bold-constructs t)
-  (setq modus-themes-italic-constructs t)
-  (setq modus-themes-mixed-fonts t))
-
-(use-package standard-themes
-  :demand t)
-
 (defun shiro-set-font (&optional frame)
   "Set font families on FRAME."
   (let ((font-height 120))
