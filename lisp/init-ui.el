6fd99694b99875be8afa803ca3fa934fd0d72613
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-09-01 00:08:42 +0800

    update init-ui.el

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -23,12 +23,6 @@
 
 (use-package spacious-padding
   :init (spacious-padding-mode)
-  :config (shiro--mode-line-update-right-align-edge)
-  :preface (defun shiro--mode-line-update-right-align-edge ()
-             "Update `right-align-edge' based on `spacious-padding-mode'."
-             (setq mode-line-right-align-edge
-                   (if spacious-padding-mode 'window 'right-margin)))
-  :hook (spacious-padding-mode-hook . shiro--mode-line-update-right-align-edge)
   :custom (spacious-padding-widths
            '(:internal-border-width 16 :right-divider-width 1 :mode-line-width 8)))
 
