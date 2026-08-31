f0b8fe02e72c336022b85b86803ddbe157e15be4
shiro <45232262+simplyshiro@users.noreply.github.com>
2026-08-31 22:40:35 +0800

    create init-vc.el

---
+++ lisp/init-vc.el
@@ -0,0 +1,17 @@
+;;; init-vc.el --- Init VC -*- lexical-binding: t; -*-
+
+;; Author: simplyshiro
+;; URL: https://github.com/simplyshiro/.emacs.d
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package magit)
+
+(use-package diff-hl
+  :init (global-diff-hl-mode))
+
+(provide 'init-vc)
+
+;;; init-vc.el ends here
